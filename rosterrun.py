623ea12d2a32ab022ccde19a25281e41419c122a
Andy Gee <andygee@gmail.com>
2013-11-25 01:35:27 -0800

    added party combinations

--- rosterrun.py
+++ rosterrun.py
@@ -19,15 +19,39 @@ app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class PartyCombination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    g_spreadsheet_id = db.Column(db.String(80), unique=True)
+    g_worksheet_id = db.Column(db.String(80), unique=True)
+    partyIndex = db.Column(db.Integer, unique=False)
+    instanceName = db.Column(db.String(80), unique=False)
+    playerName = db.Column(db.String(80), unique=False)
+    name = db.Column(db.String(80), unique=False)
+    class = db.Column(db.String(80), unique=False)
+    rolename = db.Column(db.String(80), unique=False)
+
+    def __init__(self, spreadsheet_id, worksheet_id, pIndex, iName, pName, cName, cClass, rName):
+        self.g_spreadsheet_id = spreadsheet_id
+        self.g_worksheet_id = worksheet_id
+	self.partyIndex = pIndex
+  	self.instanceName = iName
+	self.playerName = pName
+	self.name = cName
+	self.class = cClass
+	self.rolename = rName
+
+    def __repr__(self):
+        return '<PartyCombination %r>' % self.playerName
+
 sched = scheduler()
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+#def connect_db():
+#    return sqlite3.connect(app.config['DATABASE'])
+#def init_db():
+#    with closing(connect_db()) as db:
+#        with app.open_resource('schema.sql', mode='r') as f:
+#            db.cursor().executescript(f.read())
+#        db.commit()
 
 def resetParameters():
     session['user'] = None
@@ -36,13 +60,13 @@ def resetParameters():
 
 @app.before_request
 def before_request():
-    g.db = connect_db()
+    #g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
+    #db = getattr(g, 'db', None)
+    #if db is not None:
+        #db.close()
 
 @app.route('/')
 def show_entries():
@@ -54,9 +78,11 @@ def show_entries():
       #print g_s_id, g_w_id
       session['g_spreadsheet_id'] = g_s_id
       session['g_worksheet_id'] = g_w_id    
-      cur = g.db.execute('select partyIndex, instanceName, playername, name, class, rolename from combinations where g_spreadsheet_id = ? and g_worksheet_id = ? order by partyIndex, instanceName desc', \
-        (session['g_spreadsheet_id'], session['g_worksheet_id']))
-      availableParties = [Combination(row[0], row[1], row[2], row[3], row[4], row[5]) for row in cur.fetchall()]
+      cur = PartyCombination.query.filter_by(g_spreadsheet_id=session['g_spreadsheet_id'] and g_worksheet_id=session['g_worksheet_id']) 
+      #g.db.execute('select partyIndex, instanceName, playername, name, class, rolename from combinations where g_spreadsheet_id = ? and g_worksheet_id = ? order by partyIndex, instanceName desc', \
+      #  (session['g_spreadsheet_id'], session['g_worksheet_id']))
+      #availableParties = [Combination(row[0], row[1], row[2], row[3], row[4], row[5]) for row in cur.fetchall()]
+      availableParties = [Combination(c.partyIndex, c.instanceName, c.playerName, c.name, c.class, c.rolename) for c in cur]
     #print 'availble parties', availableParties
     return render_template('show_entries.html', combinations=availableParties)
 
@@ -69,9 +95,12 @@ def run_calculation():
         flash('Must include relevant document name')
         return redirect(url_for('show_entries'))
 
-    g.db.execute('delete from combinations where g_spreadsheet_id = ? and g_worksheet_id = ?', \
-      (session['g_spreadsheet_id'], session['g_worksheet_id']))
-    g.db.commit()
+    cur = PartyCombination.query.filter_by(g_spreadsheet_id=session['g_spreadsheet_id'] and g_worksheet_id=session['g_worksheet_id']) 
+    [db.session.delete(c) for c in cur]  
+    db.session.commit()
+    #g.db.execute('delete from combinations where g_spreadsheet_id = ? and g_worksheet_id = ?', \
+    #  (session['g_spreadsheet_id'], session['g_worksheet_id']))
+    #g.db.commit()
 
     (g_s_id, g_w_id) = testConnectToSpreadsheetsService(session['user'], session['pw'], session['doc'])
     #print g_s_id, g_w_id
@@ -86,9 +115,11 @@ def run_calculation():
     #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
     for i in range(0, len(parties) - 1):
       #print [(c.PartyIndex, c.InstanceName, c.PlayerName, c.CharacterName, c.CharacterClass, c.RoleName) for c in parties[i]]
-      [g.db.execute('insert into combinations (g_spreadsheet_id, g_worksheet_id, partyIndex, instanceName, playername, name, class, rolename) values (?, ?, ?, ?, ?, ?, ?, ?)', \
-        (session['g_spreadsheet_id'], session['g_worksheet_id'], c.PartyIndex, c.InstanceName, c.PlayerName, c.CharacterName, c.CharacterClass, c.RoleName)) for c in parties[i]]
-    g.db.commit()
+      [db.session.add(PartyCombination(session['g_spreadsheet_id'], session['g_worksheet_id'], c.PartyIndex, c.InstanceName, c.PlayerName, c.CharacterName, c.CharacterClass, c.RoleName)) for c in parties[i]
+      #[g.db.execute('insert into combinations (g_spreadsheet_id, g_worksheet_id, partyIndex, instanceName, playername, name, class, rolename) values (?, ?, ?, ?, ?, ?, ?, ?)', \
+        #(session['g_spreadsheet_id'], session['g_worksheet_id'], c.PartyIndex, c.InstanceName, c.PlayerName, c.CharacterName, c.CharacterClass, c.RoleName)) for c in parties[i]]
+#    g.db.commit()
+    db.session.commit()
     flash('Calculation finished')
     return redirect(url_for('show_entries'))
 
@@ -103,9 +134,13 @@ def reset():
     sched.testConnectToSpreadsheetsService(session['user'], session['pw'], session['doc'])
     session['g_spreadsheet_id'] = sched.g_spreadsheet_id
     session['g_worksheet_id'] = sched.g_worksheet_id    
-    g.db.execute('delete from combinations where g_spreadsheet_id = ? and g_worksheet_id = ?', \
-      (session['g_spreadsheet_id'], session['g_worksheet_id']))
-    g.db.commit()
+#    g.db.execute('delete from combinations where g_spreadsheet_id = ? and g_worksheet_id = ?', \
+#      (session['g_spreadsheet_id'], session['g_worksheet_id']))
+#    g.db.commit()
+    cur = PartyCombination.query.filter_by(g_spreadsheet_id=session['g_spreadsheet_id'] and g_worksheet_id=session['g_worksheet_id']) 
+    [db.session.delete(c) for c in cur]  
+    db.session.commit()
+
     flash('Reset party combinations')
     return redirect(url_for('show_entries'))
 
