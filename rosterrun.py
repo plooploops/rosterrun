84680a348fe17d1a638e2f38ac1f446ae8ace83c
Andy Gee <andygee@gmail.com>
2014-01-08 02:39:38 -0800

    changed for background worker

--- rosterrun.py
+++ rosterrun.py
@@ -55,7 +55,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 q = Queue(connection=conn, default_timeout=3600)
-
+job 
 class PartyCombo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     g_spreadsheet_id = db.Column(db.String(80))
@@ -127,7 +127,8 @@ def show_entries():
  
     action = None
     availableParties = []
- 
+    checkCalculation()
+    
     try:
       session['doc'] = request.form['gdocname'].strip()
       action = request.form['action']
@@ -264,19 +265,23 @@ def run_calculation():
 
       session['g_spreadsheet_id'] = g_s_id
       session['g_worksheet_id'] = g_w_id
-      parties = q.enqueue(run_scheduler_OAuth, credentials, session['doc'])
-      
-      #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
-      for i in range(0, len(parties) - 1):
-        [db.session.add(PartyCombo(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.PartyIndex), str(c.InstanceName), str(c.PlayerName), str(c.CharacterName), str(c.CharacterClass), str(c.RoleName))) for c in parties[i]]
-     
-      db.session.commit()
-      flash('Calculation finished')
+      job = q.enqueue(run_scheduler_OAuth, credentials, session['doc'])
+
+      checkCalculation()      
     #except:
     #  print 'error running calculation'
       return redirect(url_for('show_entries'))
     
- 
+def checkCalculation():
+  if job.Result is not None:
+    parties = job.result      
+    #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
+    for i in range(0, len(parties) - 1):
+      [db.session.add(PartyCombo(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.PartyIndex), str(c.InstanceName), str(c.PlayerName), str(c.CharacterName), str(c.CharacterClass), str(c.RoleName))) for c in parties[i]]
+     
+    db.session.commit()
+    flash('Calculation finished')
+
 @app.route('/reset', methods=['POST'])
 def reset():
     try:
