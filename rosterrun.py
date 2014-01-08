e7d4fd50017bd22a1c838da2a826ee3ce16631e7
Andy Gee <andygee@gmail.com>
2014-01-07 20:22:45 -0800

    spreadsheet retrieval

--- rosterrun.py
+++ rosterrun.py
@@ -180,197 +180,218 @@ def show_entries():
 
 @app.route('/import_characters', methods=['POST'])
 def import_characters():
-    if not session.get('logged_in'):
-      #abort(401)
-      flash('Please login again')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
+    try:
+      if not session.get('logged_in'):
+        #abort(401)
+        flash('Please login again')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
     
-    if(len(session['doc']) <= 0):
-        flash('Must include relevant document name')
-        return redirect(url_for('show_entries'))
+      if(len(session['doc']) <= 0):
+          flash('Must include relevant document name')
+          return redirect(url_for('show_entries'))
 
-    if('g_spreadsheet_id' in session.keys() and 'g_worksheet_id' in session.keys()):
-      cur = MappedCharacter.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
-      [db.session.delete(c) for c in cur]  
-      db.session.commit()
+      if('g_spreadsheet_id' in session.keys() and 'g_worksheet_id' in session.keys()):
+        cur = MappedCharacter.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
+        [db.session.delete(c) for c in cur]  
+        db.session.commit()
     
-    loginConfiguration(session['user'])
-    user = users.get_current_user()
-    storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
-    credentials = storage.get()   
-    if credentials is None:
-      flash('Please login again')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
-    
-    (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
-    if(g_s_id == -1 or g_w_id == -1):
-      flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
-      return redirect(url_for('show_entries'))
-
-    session['g_spreadsheet_id'] = g_s_id
-    session['g_worksheet_id'] = g_w_id
-    basequests = ['tripatriateunionsfeud', 'attitudetothenewworld', 'ringofthewiseking', 'newsurroundings', 'twotribes', 'pursuingrayanmoore', 'reportfromthenewworld', 'guardianofyggsdrasilstep9', 'onwardtothenewworld']
-    chars = initializeDataOAuth(credentials, session['doc'], basequests)
-    print 'FOUND %s CHARS' % len(chars)
-    #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
-    [db.session.add(MappedCharacter(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.Class), str(c.Name), str(c.Role.Name), str('|'.join(c.Quests)), str(c.LastRun), str(c.PlayerName), str(c.Present))) for c in chars]
+      loginConfiguration(session['user'])
+      user = users.get_current_user()
+      storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
+      credentials = storage.get()   
+      if credentials is None:
+        flash('Please login again')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
      
-    db.session.commit()
-    flash('Import finished')
-    
+      (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
+      if(g_s_id == -1 or g_w_id == -1):
+        flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
+        return redirect(url_for('show_entries'))
+ 
+      session['g_spreadsheet_id'] = g_s_id
+      session['g_worksheet_id'] = g_w_id
+      basequests = ['tripatriateunionsfeud', 'attitudetothenewworld', 'ringofthewiseking', 'newsurroundings', 'twotribes', 'pursuingrayanmoore', 'reportfromthenewworld', 'guardianofyggsdrasilstep9', 'onwardtothenewworld']
+      chars = initializeDataOAuth(credentials, session['doc'], basequests)
+      print 'FOUND %s CHARS' % len(chars)
+      #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
+      [db.session.add(MappedCharacter(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.Class), str(c.Name), str(c.Role.Name), str('|'.join(c.Quests)), str(c.LastRun), str(c.PlayerName), str(c.Present))) for c in chars]
+     
+      db.session.commit()
+      flash('Import finished')
+    except:
+      print 'error importing'
     return redirect(url_for('show_entries'))
 
 @app.route('/runcalc', methods=['POST'])
 def run_calculation():
-    if not session.get('logged_in'):
-      #abort(401)
-      flash('Please login again')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
+    try:
+      if not session.get('logged_in'):
+        #abort(401)
+        flash('Please login again')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
     
-    if(len(session['doc']) <= 0):
-        flash('Must include relevant document name')
+      if(len(session['doc']) <= 0):
+          flash('Must include relevant document name')
+          return redirect(url_for('show_entries'))
+
+      if('g_spreadsheet_id' in session.keys() and 'g_worksheet_id' in session.keys()):
+        cur = PartyCombo.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
+        [db.session.delete(c) for c in cur]  
+        db.session.commit()
+    
+      loginConfiguration(session['user'])
+      user = users.get_current_user()
+      storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
+      credentials = storage.get()   
+      if credentials is None:
+        flash('Please login again')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
+    
+      (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
+      if(g_s_id == -1 or g_w_id == -1):
+        flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
         return redirect(url_for('show_entries'))
 
-    if('g_spreadsheet_id' in session.keys() and 'g_worksheet_id' in session.keys()):
-      cur = PartyCombo.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
-      [db.session.delete(c) for c in cur]  
-      db.session.commit()
-    
-    loginConfiguration(session['user'])
-    user = users.get_current_user()
-    storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
-    credentials = storage.get()   
-    if credentials is None:
-      flash('Please login again')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
-    
-    (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
-    if(g_s_id == -1 or g_w_id == -1):
-      flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
-      return redirect(url_for('show_entries'))
-
-    session['g_spreadsheet_id'] = g_s_id
-    session['g_worksheet_id'] = g_w_id
-    parties = run_scheduler_OAuth(credentials, session['doc'])
-    print 'FOUND %s PARTIES' % len(parties)
-    #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
-    for i in range(0, len(parties) - 1):
-      [db.session.add(PartyCombo(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.PartyIndex), str(c.InstanceName), str(c.PlayerName), str(c.CharacterName), str(c.CharacterClass), str(c.RoleName))) for c in parties[i]]
+      session['g_spreadsheet_id'] = g_s_id
+      session['g_worksheet_id'] = g_w_id
+      parties = run_scheduler_OAuth(credentials, session['doc'])
+      print 'FOUND %s PARTIES' % len(parties)
+      #parties combinations have [PartyIndex,InstanceName,PlayerName,CharacterName,CharacterClass,RoleName']
+      for i in range(0, len(parties) - 1):
+        [db.session.add(PartyCombo(str(session['g_spreadsheet_id']), str(session['g_worksheet_id']), str(c.PartyIndex), str(c.InstanceName), str(c.PlayerName), str(c.CharacterName), str(c.CharacterClass), str(c.RoleName))) for c in parties[i]]
      
-    db.session.commit()
-    flash('Calculation finished')
-    
+      db.session.commit()
+      flash('Calculation finished')
+    except:
+      print 'error running calculation'
     return redirect(url_for('show_entries'))
 
 @app.route('/reset', methods=['POST'])
 def reset():
-    if not session.get('logged_in'):
-      #abort(401)
-      flash('Please login again')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
+    try:
+      if not session.get('logged_in'):
+        #abort(401)
+        flash('Please login again')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
 
-    if(len(session['doc']) <= 0):
-      flash('Must include relevant document name')
-      return redirect(url_for('show_entries'))
-
-    loginConfiguration(session['user'])
-    user = users.get_current_user()
-    storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
-    credentials = storage.get()    
-    if credentials is None:
-      flash('Cannot log in to spreadsheet')
-      session.pop('logged_in', None)
-      return redirect(url_for('login'))
+      if(len(session['doc']) <= 0):
+        flash('Must include relevant document name')
+        return redirect(url_for('show_entries'))
+  
+      loginConfiguration(session['user'])
+      user = users.get_current_user()
+      storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
+      credentials = storage.get()    
+      if credentials is None:
+        flash('Cannot log in to spreadsheet')
+        session.pop('logged_in', None)
+        return redirect(url_for('login'))
     
-    (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
-    if(g_s_id == -1 or g_w_id == -1):
-      flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
-      return redirect(url_for('show_entries'))
-
-    session['g_spreadsheet_id'] = g_s_id
-    session['g_worksheet_id'] = g_w_id
+      (g_s_id, g_w_id) = testConnectToSpreadsheetsServiceOAuth(credentials, session['doc'])
+      if(g_s_id == -1 or g_w_id == -1):
+        flash('Cannot connect to google document.  Please check spreadsheet name, google credentials and connectivity.')
+        return redirect(url_for('show_entries'))
+  
+      session['g_spreadsheet_id'] = g_s_id
+      session['g_worksheet_id'] = g_w_id
+         
+      cur = PartyCombo.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
        
-    cur = PartyCombo.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
-      
-    [db.session.delete(c) for c in cur]  
-    db.session.commit()
+      [db.session.delete(c) for c in cur]  
+      db.session.commit()
 
-    cur = MappedCharacter.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
-    [db.session.delete(c) for c in cur]  
-    db.session.commit()  
+      cur = MappedCharacter.query.filter_by(g_spreadsheet_id=str(session['g_spreadsheet_id']), g_worksheet_id=str(session['g_worksheet_id'])) 
+      [db.session.delete(c) for c in cur]  
+      db.session.commit()  
     
-    flash('Reset party combinations')
+      flash('Reset party combinations')
+    except:
+      print 'error reseting'
       
     return redirect(url_for('show_entries')) 
 
 @app.route('/auth_return', methods=['GET', 'POST'])
 def oauth2callback():
-    codeValue = parseUrl(request, 'code')
-    if len(codeValue) > 0:
-      #Store credentials
-      credentials = flow.step2_exchange(codeValue)
-      user = users.get_current_user()
-      storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
-      storage.put(credentials)    
-      session['logged_in'] = True
-      #credentials stored
-      flash('You were logged in')
-      return redirect(url_for('show_entries'))
+    try:
+      codeValue = parseUrl(request, 'code')
+      if len(codeValue) > 0:
+        #Store credentials
+        credentials = flow.step2_exchange(codeValue)
+        user = users.get_current_user()
+        storage = StorageByKeyName(CredentialsModel, str(user), 'credentials')
+        storage.put(credentials)    
+        session['logged_in'] = True
+        #credentials stored
+        flash('You were logged in')
+        return redirect(url_for('show_entries'))
+    except: 
+      print 'error with oauth2callback'
  
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    resetParameters()
-    error = None    
+    try:
+      resetParameters()
+      error = None    
     
-    if request.method == 'POST':
-        if len(request.form['username']) == 0:
-            error = 'Invalid username'
-        else:
-            username = request.form['username'].strip()
-            session['user'] = username
+      if request.method == 'POST':
+          if len(request.form['username']) == 0:
+              error = 'Invalid username'
+          else:
+              username = request.form['username'].strip()
+              session['user'] = username
 
-	    loginConfiguration(username)
-            flow = OAuth2WebServerFlow(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=SCOPES, redirect_uri=REDIRECT_URI)
-            #print flow
-#            user = users.get_current_user()
-#            print user.user_id()
-#	    memcache.set(user.user_id(), pickle.dumps(flow))
+              loginConfiguration(username)
+              flow = OAuth2WebServerFlow(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=SCOPES, redirect_uri=REDIRECT_URI)
+              #print flow
+  #            user = users.get_current_user()
+  #            print user.user_id()
+       #       memcache.set(user.user_id(), pickle.dumps(flow))
             
-            auth_uri = flow.step1_get_authorize_url()
-            #print auth_uri
+              auth_uri = flow.step1_get_authorize_url()
+              #print auth_uri
             
-#            flow = pickle.loads(memcache.get(user.user_id()))            
-            #print flow
+    #          flow = pickle.loads(memcache.get(user.user_id()))            
+              #print flow
             
-            return redirect(auth_uri)
-            #
-            #If the user has previously granted your application access, the authorization server immediately redirects again to redirect_uri. If the user has not yet granted access, the authorization server asks them to grant your application access. If they grant access, they get redirected to redirect_uri with a code query string parameter similar to the following:
-	    #
-	    #http://example.com/auth_return/?code=kACAH-1Ng1MImB...AA7acjdY9pTD9M
-	    #If they deny access, they get redirected to redirect_uri with an error query string parameter similar to the following:
-	    #
-	    #http://example.com/auth_return/?error=access_denied
-	    #
-    return render_template('login.html', error=error)
+              return redirect(auth_uri)
+              #
+              #If the user has previously granted your application access, the authorization server immediately redirects again to redirect_uri. If the user has not yet granted access, the authorization server asks them to grant your application access. If they grant access, they get redirected to redirect_uri with a code query string parameter similar to the following:
+	      #
+	      #http://example.com/auth_return/?code=kACAH-1Ng1MImB...AA7acjdY9pTD9M
+	      #If they deny access, they get redirected to redirect_uri with an error query string parameter similar to the following:
+	      #
+	      #http://example.com/auth_return/?error=access_denied
+	      #
+      return render_template('login.html', error=error)
+    except:
+      print 'error with login'
+      return render_template('login.html', error='Please give permissions to log in')
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    try:
+      session.pop('logged_in', None)
+      flash('You were logged out')
+      return redirect(url_for('show_entries'))
+    except:
+      print 'error with logout'
+      return redirect(url_for('show_entries'))  
 
 def loginConfiguration(username, userid=1):
-  print 'running with user %s ' % username
-  os.environ['USER_EMAIL'] = username
-  #can this a default?
-  os.environ['USER_ID'] = str(userid)
-  os.environ['AUTH_DOMAIN'] = 'testbed'
-  os.environ['APPLICATION_ID'] = 'roster run'
+  try:
+    print 'running with user %s ' % username
+    os.environ['USER_EMAIL'] = username
+    #can this a default?
+    os.environ['USER_ID'] = str(userid)
+    os.environ['AUTH_DOMAIN'] = 'testbed'
+    os.environ['APPLICATION_ID'] = 'roster run'
+  except:
+    print 'error with login configuration'
 
 if __name__ == "__main__":
   db.create_all()
