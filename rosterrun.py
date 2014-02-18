55bb77b83e0bc561006ddde74a1919b6fe93b171
Andy Gee <andygee@gmail.com>
2014-02-18 02:09:16 -0800

    bar chart test

--- rosterrun.py
+++ rosterrun.py
@@ -395,7 +395,7 @@ def item_history():
   #format data
   mrs = [MarketResult(m.itemid, m.name, m.cards.split(','), m.price, m.amount, m.title, m.vendor, m.coords, m.date) for m in mr]
   
-  projected_results = [(convert_to_key(m.itemid, m.name), {'value':(m.date, int(m.price)), 'label':convert_to_key(m.itemid, m.name, m.cards)}) for m in mrs]  
+  projected_results = [(convert_to_key(m.name), {'value':(m.date, int(m.price)), 'label':convert_to_key(m.itemid, m.name, m.cards)}) for m in mrs]  
   #print projected_results
 
   res_dict = {}
@@ -407,11 +407,11 @@ def item_history():
         res_dict[key] = [pr[1]]
   
   datey = pygal.DateY(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Price', x_title='Date', x_labels_major_every=2)
-  datey.title = "Market History Overview"
+  datey.title = "Market History Overview for %s" % val
   datey.x_label_format = "%Y-%m-%d"
   [datey.add(k, res_dict[k]) for k in res_dict.keys()]
   
-  projected_results = [(convert_to_key(m.itemid, m.name, m.cards), {'value': int(m.price), 'label':convert_to_key(m.itemid, m.name, m.cards, m.date.strftime('%d, %b %Y'))}) for m in mrs]
+  projected_results = [(convert_to_key(m.name, m.cards), {'value': int(m.price), 'label':convert_to_key(m.itemid, m.name, m.cards, m.date.strftime('%d, %b %Y'))}) for m in mrs]
   print projected_results
   res_dict = {}
   for key, group in groupby(projected_results, lambda x: x[0]):
@@ -424,7 +424,7 @@ def item_history():
   print res_dict
   histchart = datey.render()
   
-  bar_chart = pygal.Bar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Price', x_title='Item', x_labels_major_every=2)
+  bar_chart = pygal.Bar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Price', x_title='Item %s' % val, x_labels_major_every=2)
   bar_chart.title = "Market History Overview"
   [bar_chart.add(k, res_dict[k]) for k in res_dict.keys()]
   barhistchart = bar_chart.render()
