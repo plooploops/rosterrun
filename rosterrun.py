cbe48d9a25cc4907e86b51391c6a579620599bfb
Andy Gee <andygee@gmail.com>
2014-02-19 04:16:47 -0800

    testing prices volumes

--- rosterrun.py
+++ rosterrun.py
@@ -429,7 +429,7 @@ def item_current_results():
     print 'value not found'
     
   d = datetime.now()
-  latest_item = MappedMarketResult.query.filter(MappedMarketResult.itemid==val).order_by(MappedMarketResult.date.desc()).all()
+  latest_item = MappedMarketResult.query.order_by(MappedMarketResult.date.desc()).all()
   if len(latest_item) > 0:
     d = latest_item[0].date
   
@@ -454,6 +454,8 @@ def item_current_results():
   datey.title = "Current Prices for %s" % val
   datey.x_label_format = "%Y-%m-%d"
   [datey.add(k, res_dict[k]) for k in res_dict.keys()]
+  
+  print res_dict
   
   pricechart = datey.render()
   
@@ -487,7 +489,6 @@ def item_current_results():
   
   bar_chart = pygal.StackedBar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Quantity', x_title='Item %s' % val, x_labels_major_every=2)
   bar_chart.title = "Current Selling Volume for %s" % val
-  bar_chart.x_labels = dates
   [bar_chart.add(k, res_dict[k]) for k in res_dict.keys()]
 
   volumechart = bar_chart.render()
@@ -562,7 +563,6 @@ def item_history():
         
   bar_chart = pygal.StackedBar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Quantity', x_title='Item %s' % val, x_labels_major_every=2)
   bar_chart.title = "Historical Selling Volume for %s" % val
-  bar_chart.x_labels = dates
   [bar_chart.add(k, res_dict[k]) for k in res_dict.keys()]
 
   volumechart = bar_chart.render()
