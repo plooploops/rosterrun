fbec1fdf422c934a878c6b35b0f380f0aa47c67e
Andy Gee <andygee@gmail.com>
2014-02-19 02:54:37 -0800

    prices and volumes

--- rosterrun.py
+++ rosterrun.py
@@ -466,8 +466,8 @@ def item_current_results():
     date_index = 0
     for pr in group:
       val = None
-      pr[0].split(' ')[1]
-      if pr[0].split(' ')[1] == dates[date_index]:
+      print key.split(' ')[1]
+      if key.split(' ')[1] == dates[date_index]:
         val = pr[1]  
       if key in res_dict.keys():
         res_dict[key].append(val)
@@ -539,8 +539,8 @@ def item_history():
     date_index = 0
     for pr in group:
       val = None
-      print pr[0].split(' ')[1]
-      if pr[0].split(' ')[1] == dates[date_index]:
+      print key.split(' ')[1]
+      if key.split(' ')[1] == dates[date_index]:
         val = pr[1]  
       if key in res_dict.keys():
         res_dict[key].append(val)
