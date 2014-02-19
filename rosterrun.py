ab223328f3406c096ec50c668ec1a2eb3c66d347
Andy Gee <andygee@gmail.com>
2014-02-19 04:05:40 -0800

    testing prices volumes

--- rosterrun.py
+++ rosterrun.py
@@ -487,7 +487,7 @@ def item_current_results():
   
   bar_chart = pygal.StackedBar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Quantity', x_title='Item %s' % val, x_labels_major_every=2)
   bar_chart.title = "Current Selling Volume for %s" % val
-  bar_chart.x_labels = [d.strftime('%d,  %b %Y') for d in dates]
+  bar_chart.x_labels = dates
   [bar_chart.add(k, res_dict[k]) for k in res_dict.keys()]
 
   volumechart = bar_chart.render()
@@ -562,7 +562,7 @@ def item_history():
         
   bar_chart = pygal.StackedBar(x_label_rotation=20, no_data_text='No result found', disable_xml_declaration=True, dots_size=5, legend_font_size=18, legend_box_size=18, value_font_size=16, label_font_size=14, tooltip_font_size=18, human_readable=True, stroke=False, style=LightStyle, truncate_legend=200, truncate_label=200, legend_at_bottom=True, y_title='Quantity', x_title='Item %s' % val, x_labels_major_every=2)
   bar_chart.title = "Historical Selling Volume for %s" % val
-  bar_chart.x_labels = [d.strftime('%d,  %b %Y') for d in dates]
+  bar_chart.x_labels = dates
   [bar_chart.add(k, res_dict[k]) for k in res_dict.keys()]
 
   volumechart = bar_chart.render()
