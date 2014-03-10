3b15695f83c2b70ec472eb140baef5d2e64f6532
Andy Gee <andygee@gmail.com>
2014-03-10 01:52:34 -0700

    add run in progress

--- rosterrun.py
+++ rosterrun.py
@@ -893,7 +893,7 @@ def add_run():
     #check if the run is already part of the db before adding again else edit
     s3 = boto.connect_s3(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
     bucket = s3.get_bucket(os.environ['S3_BUCKET_NAME'])
-    expires_in_seconds = os.environ['S3_EXPIRES_IN_SECONDS']
+    expires_in_seconds = int(os.environ['S3_EXPIRES_IN_SECONDS'])
     
     name = request.form['nrunname']
     file = request.files['nrunscreenshot']
