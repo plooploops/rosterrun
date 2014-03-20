aaabe546415d1b2b054568ded8641e0bf33f13f9
Cheng <humphreycheng0481@gmail.com>
2014-03-19 23:36:30 -0500

    Update items_map.py
    added ET - Naght Sieger, haven't organized the list of items yet.

--- items_map.py
+++ items_map.py
@@ -48,7 +48,7 @@ instance_mob_item_mapping = [
   ('Bio - Lord Knight', 10, 1646, 'Lord Knight Seyren', [(1132, .25), (2342, .35), (2412, .9), (1470, .35), (1469, .3), (1166, .25), (1415, .15), (4357, .0003)], biolabQuestsExternal),
   ('Bio - Lord Knight', 10, 1646, 'Lord Knight Seyren', [(1132, .25), (2342, .35), (2412, .9), (1470, .35), (1469, .3), (1166, .25), (1415, .15), (4357, .0003)], biolabQuestsExternal),
   ('Bio - Lord Knight', 10, 1646, 'Lord Knight Seyren', [(1132, .25), (2342, .35), (2412, .9), (1470, .35), (1469, .3), (1166, .25), (1415, .15), (4357, .0003)], biolabQuestsExternal),
-]
+  ('Endless Tower - Naght Sieger', 10, 1956, 'Naght Sieger', [{13412, .5), (13413, .5), (2542, .5), (5017, .90), (616, 1.0), (2514, .9), (7294, 1.0)], EndlessTowerExternal),
 
 #mobs
 #2022 - nidd
@@ -58,11 +58,12 @@ instance_mob_item_mapping = [
 #1956 - naght sieger
 #1957 - Entweihen Crothen
 #1650 - Sniper Cecil
-#1649 - High Priest Margaretha
+#1649 - High Priest Margaretha,
 #1651 - High Wizard Kathryne
 #1648 - Whitesmith Howard
 #1647 - Assassin Cross Eremes
 #1646 - Lord Knight Seyren
+#1956 - Naght Sieger
 
 mobs_in_run = {
   'Niddhogg' : [2022],
@@ -122,81 +123,4 @@ search_items = {
   7754 : "Broken Crown",
   2423 : "Variant Shoes", 
   1565 : "Book of the Dead",
-  2000 : "Staff of Destruction",
-  2702 : "Horn of the Buffalo",
-  985 : "Elunium",
-  742 : "Chonchon Doll",
-  4145 : "Berzebub Card",
-  994 : "Flame Heart",
-  2677 : "Spiritual Ring",
-  12103 : "Bloody Branch",
-  13017 : "Ice Pick [1]",
-  1471 : "Hellfire",
-  1133 : "Fireblend",
-  2345 : "Lucius's Fierce Armor of Volcano [1]",
-  5420 : "Mask Of Ifrit",
-  4430 : "Ifrit Card",
-  13412 : "Naght Seiger Twin Blade(B) [3]",
-  13413 : "Naght Seiger Twin Blade(R) [3]",
-  2542 : "Naght Seiger Flame Manteau [1]",
-  5017 : "Bone Helm",
-  616 : "Old Card Album",
-  2514 : "Pauldron [1]",
-  7294 : "Turquoise",
-  1636 : "Dark Thorn Staff",
-  1631 : "Holy Stick [1]",
-  2513 : "Heavenly Maiden Robe [1]", 
-  1624 : "Lich's Bone Wand [2]",
-  1618 : "Survivor's Rod [1]",
-  7291 : "Agate",
-  1228 : "Combat Knife",
-  1236 : "Sucsamad",
-  617 : "Old Purple Box",
-  1234 : "Moonlight Dagger",
-  1237 : "Grimtooth",
-  1720 : "Rudra Bow",
-  1724 : "Dragon Wing",
-  4367 : "Sniper Card",
-  1814 : "Berserk",
-  2615 : "Safety Ring",
-  1557 : "Book of the Apocalypse",
-  1527 : "Quadrille",
-  1528 : "Grand Cross",
-  1560 : "Sage's Diary [2]",
-  4363 : "High Priest Card",
-  1138 : "Mysteltainn",
-  1140 : "Byeollungum",
-  2318 : "Lord's Clothes [1]",
-  1365 : "Sabbath", 
-  1364 : "Great Axe",
-  1369 : "Guillotine",
-  1368 : "Tomahawk",
-  4361 : "Whitesmith Card",
-  1132 : "Edge",
-  2342 : "Legion Plate Armor [1]",
-  2412 : "Greaves [1]",
-  1470 : "Brionac",
-  1469 : "Longinus's Spear",
-  1166 : "Dragon Slayer",
-  1415 : "Brocca",
-  4357 : "Lord Knight Card",
-  1241 : "Cursed Dagger",
-  1242 : "Dagger of Counter",
-  2616 : "Critical Ring",
-  2343 : "Robe of Cast",
-  1618 : "Survivor's Rod [1]",
-  2319 : "Glittering Jacket [1]",
-  4365 : "High Wizard Card",
-  1230 : "Ice Pick",
-  1233 : "Exorciser",
-  1232 : "Assassin Dagger",
-  1265 : "Bloody Roar",
-  13002 : "Ginnungagap",
-  4359 : "Assassin Cross Card"  
-}
-
-test_file = "requests_results_sell.html"
-test_items = { 
-  8900 : "Talon Coin", 
-  2554 : "Nydhorgg's Shadow Garb" 
-}
+  2000 : "Staff of D
