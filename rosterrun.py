b6bdbf646fc16c78ab57f75f2c46e854c06e0aa2
Andy Gee <andygee@gmail.com>
2014-03-28 02:16:51 -0700

    guild points calculation

--- rosterrun.py
+++ rosterrun.py
@@ -2032,6 +2032,7 @@ def CalculatePoints(run = None, mobs_killed = [], players = [], market_results =
   #assign points
   
   #if this is reassignment
+  mps = MappedPlayer.query.filter(MappedPlayer.id.in_(players)).all()
   player_ids = [p for p in players]
   relevant_runs_query = db.session.query(RunCredit, MappedPlayer, MappedGuildPoint, MappedRun).join(MappedPlayer).join(MappedGuildPoint).join(MappedRun).filter(MappedRun.success == True).filter(MappedRun.id==run.id).filter(RunCredit.factor > 0).filter(MappedPlayer.id.in_(player_ids))
   if relevant_runs_query.count() > 0:
@@ -2047,7 +2048,7 @@ def CalculatePoints(run = None, mobs_killed = [], players = [], market_results =
   else:
     #if this is a new run
     mapped_points = []
-    for p in players:
+    for p in mps:
       mgp = MappedGuildPoint(points_per_player)
       mapped_points.append(mgp)
       p.Points.append(mgp)
