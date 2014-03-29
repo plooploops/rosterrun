2312653c86da5112b434d34e39904f289bd9abb3
Andy Gee <andygee@gmail.com>
2014-03-29 15:30:09 -0700

    looking up item name from server

--- marketscrape.py
+++ marketscrape.py
@@ -217,13 +217,15 @@ class MarketScraper:
     items_results = { }
     for i in items_to_search:
       sell_url = base_url %  i
+      print sell_url
       #run query
       sell_r = requests.post(sell_url, cookies=self.cookies)
       #load results into tree
       tree = html.fromstring(sell_r.content)
       #find search results
+      print 'searching for itemid %s' % i
       vals = tree.xpath("//table[@class='table_data table_narrow']/tbody/tr/td[@colspan='8']")
-      
+      print vals
       val_found = str.join('', [c.strip() for c in vals[0].itertext()]).strip()
       #item name - item id - (item name)
       split_val = val_found.split('-')
