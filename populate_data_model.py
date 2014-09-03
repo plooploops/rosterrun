6635a1c194d6da7c394f5e13eb30d61e9c5bd6c7
plooploops <andygee@gmail.com>
2014-09-02 23:20:36 -0700

    test db update

--- populate_data_model.py
+++ populate_data_model.py
@@ -2,6 +2,15 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import Table, Column, ForeignKey, func
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.engine import reflection
+from sqlalchemy import create_engine
+from sqlalchemy.schema import (
+    MetaData,
+    Table,
+    DropTable,
+    ForeignKeyConstraint,
+    DropConstraint,
+    )
 from datetime import datetime, timedelta
 
 from items_map import *
@@ -36,6 +45,45 @@ def populate_instances(instance_name, median_party, mob_id_name_items = [], ques
   mi.mobs = mobs_for_instance
   populate_quests(mi, quests)
   db.session.commit()
+
+def db_DropEverything(db):
+  # From http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+
+  conn=db.engine.connect()
+  
+  # the transaction only applies if the DB supports
+  # transactional DDL, i.e. Postgresql, MS SQL Server
+  trans = conn.begin()
+
+  inspector = reflection.Inspector.from_engine(db.engine)
+
+  # gather all data first before dropping anything.
+  # some DBs lock after things have been dropped in 
+  # a transaction.
+  metadata = MetaData()
+
+  tbs = []
+  all_fks = []
+
+  for table_name in inspector.get_table_names()
+    fks = []
+    for fk in inspector.get_foreign_keys(table_name):
+      if not fk['name']:
+        continue
+      fks.append(
+        ForeignKeyConstraint((),(),name=fk['name'])
+        )
+    t = Table(table_name,metadata,*fks)
+    tbs.append(t)
+    all_fks.extend(fks)
+
+  for fkc in all_fks:
+    conn.execute(DropConstraint(fkc))
+
+  for table in tbs:
+    conn.execute(DropTable(table))
+
+  trans.commit()
 
 def populate_data_model():
   for imi in instance_mob_item_mapping:
